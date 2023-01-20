4b2fab2e32a6dbd4628bb6a69f42a4e22f1278bd
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-01-20 16:57:15 -0500

    new way

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import requests, json, urllib3
-
-def main():
-
-    apiKey = '3ZZ4-X0EF-NFY1-6IYY'
+import urllib.request
+def createPassManager(password_mgr):
+    username = '3ZZ4-X0EF-NFY1-6IYY'
     password = 'zqleboe1115c3h'
-
     url = 'https://rkuczer.wufoo.com/api/v3/'
+    password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, url, username, password)
+    return password_mgr
+def createAuthHandler(password_mgr, url):
+    auth_handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
 
-    response = requests.get(f"{url}forms/{password}/entries.json", params={"api_key": apiKey})
+    opener = urllib.request.build_opener(auth_handler)
+    response = opener.open(url+'forms.json')
+
+def printResponse(response):
+    print(response.read())
+    #response = requests.get(f"{url}forms/{password}/entries.json", params={"api_key": apiKey})
 
 
-    if response.status_code == 200:
-        print(response.json())
-    else:
-        print(f'Error {response.status_code}: {response.text}')
+    #if response.status_code == 200:
+    #    print(response.json())
+    #else:
+    #    print(f'Error {response.status_code}: {response.text}')
 
 
 
-main()
+createPassManager()
+createAuthHandler()
+printResponse()
