b757c5e628adc69f024bbd92cfe4a3de0f06be53
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-02-02 18:22:38 -0500

    fixed errors on computer side

--- main.py
+++ main.py
@@ -8,14 +8,11 @@ from requests.auth import HTTPBasicAuth
 url = "https://rkuczer.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
 
 
-def insert_db(cursor:sqlite3.Cursor):
-   for entry in dataParse:
-       cursor.execute('''      
-       INSERT INTO entries (EntryId, first_name, last_name, job_title, company_name, phone_num, school_id)
-    VALUES (:entryId, :first_name, :last_name, :job_title, :company_name, :phone_num, :school_id, :org_website,
-        :course_proj, :guest_speaker, :site_visit, :job_shadow, :carreer_panel, :summer_2022, :fall_2022, :spring_2023, :summer_2023, :other, :yes_no)
-        ''', entry["EntryId"], entry["Field1"], entry["Field2"], entry["Field426"], entry["Field428"], entry["Field10"],
-        entry["Field12"])
+def insert_db(cursor:sqlite3.Cursor, data_to_save: list):
+    for entry in data_to_save:
+        for key, value in entry.items():
+            cursor.execute("INSERT INTO entries (key, value) VALUES (?, ?)", (key, value))
+
 
 
 def get_wufoo_data() -> dict:
@@ -30,11 +27,13 @@ def get_wufoo_data() -> dict:
 def main():
     data = get_wufoo_data()
     data1 = data['Entries']
+    print(data1)
     file_to_save = open("info.txt", 'w')
     save_data(data1, save_file=file_to_save)
 
     conn, cursor = open_db("demo_db.sqlite")
     print(type(conn))
+    insert_db(cursor, data1)
     setup_db(cursor)
     close_db(conn)
 
@@ -60,7 +59,7 @@ def close_db(connection:sqlite3.Connection):
 
 def setup_db(cursor:sqlite3.Cursor):
    cursor.execute('''CREATE TABLE IF NOT EXISTS entries(
- EntryId INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL,
+ EntryId INTEGER NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL,
  job_title TEXT NOT NULL, company_name TEXT NOT NULL, phone_num INTEGER NOT NULL,
  school_id INTEGER NOT NULL);''')
 
