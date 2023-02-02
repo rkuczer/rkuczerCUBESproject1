7cdb2f1861d0cda246f712cddaf63d7cc9252787
rkuczer <ryan-kooz@hotmail.com>
2023-02-02 17:20:08 -0500

    fixed sprint 1 to have correct json response that was causing current errors with adding the entries to a sql database from solution

--- main.py
+++ main.py
@@ -1,48 +1,37 @@
-from secrets import apiKey
-import json
-import urllib.request
+import requests
+import sys
+from secrets import apiKey  # add a secrets file with wufoo_key='YoUr-WuFoo-KeY-Here'
+from requests.auth import HTTPBasicAuth
 
-subdomain = 'rkuczer'
-url = (f'https://{subdomain}.wufoo.com/api/v3/')
-formHash = 'zqleboe1115c3h'
-
-
-def getInfo():
-    formHash = 'zqleboe1115c3h'
-    global url
-    password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-    password_mgr.add_password(None, url, apiKey, formHash)
-    auth_handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-    opener = urllib.request.build_opener(auth_handler)
-    return opener
+# adjust this to your URL
+url = "https://rkuczer.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
 
 
-def getResponse(opener):
-    global url
-    response = opener.open(url + (f'forms/{formHash}/entries.json'))
-    if response.status == 200:
-        data = json.load(response)
-        dataParse = (json.dumps(data, indent=4, separators=('', ':')))
-    else:
-        print(f'Error {response.status_code}: {response.text}')
-        exit()
-    print(dataParse)
-    return dataParse
+def get_wufoo_data() -> dict:
+    response = requests.get(url, auth=HTTPBasicAuth(apiKey, 'pass'))
+    if response.status_code != 200:  # if we don't get an ok response we have trouble
+        print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
+        sys.exit(-1)
+    jsonresponse = response.json()
+    return jsonresponse
 
 
-def saveFile(dataParse):
-    formatData = ""
-    for entry in dataParse:
-        formatData += entry
-    with open("info.txt", "w") as file:
-        file.write(dataParse)
-    file.close()
+def main():
+    data = get_wufoo_data()
+    data1 = data['Entries']
+    file_to_save = open("info.txt", 'w')
+    save_data(data1, save_file=file_to_save)
 
 
-opener = getInfo()
+def save_data(data_to_save: list, save_file=None):
+    for entry in data_to_save:
+        for key, value in entry.items():
+            print(f"{key}: {value}", file=save_file)
+        # now print the spacer
+        print("+++++++++++++++++++++++++++++++++++++++++++++\n_______________________________________________",
+              file=save_file)
 
 
-dataParse = getResponse(opener)
-
-
-saveFile(dataParse)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
