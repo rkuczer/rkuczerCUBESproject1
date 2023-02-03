e8988dc4f4f0bf8e077bb9d47eb5232c58d62caf
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-02-03 15:44:14 -0500

    Even even more linter formatting also created 10 fake entries to begin making automated tests.

--- main.py
+++ main.py
@@ -36,12 +36,15 @@ def insert_db(cursor: sqlite3.Cursor, data1):
         updated_by = key['UpdatedBy']
         try:
             cursor.execute("INSERT INTO entries (EntryId, first_name, last_name, job_title, org_name, phone_num, "
-                           "school_id, org_site, course, speaker, siteVisit, job_shadow, carreer_panel, summer_2022, fall_2022, "
+                           "school_id, org_site, course, speaker, siteVisit, job_shadow, carreer_panel, "
+                           "summer_2022, fall_2022, "
                            "spring_2023, summer_2023, "
                            "other, permission, date_created, created_by, date_update, updated_by) "
                            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                           (EntryId, first_name, last_name, job_title, org_name, phone_num, school_id, org_site, course, speaker, siteVisit, job_shadow,
-                            carreer_panel, summer_2022, fall_2022, spring_2023, summer_2023, other, permission, date_created, created_by, date_update, updated_by))
+                           (EntryId, first_name, last_name, job_title, org_name, phone_num, school_id, org_site,
+                            course, speaker, siteVisit, job_shadow,
+                            carreer_panel, summer_2022, fall_2022, spring_2023, summer_2023, other, permission,
+                            date_created, created_by, date_update, updated_by))
         except sqlite3.IntegrityError:
             print(f"Form submission with entryID={EntryId} and submission_date={date_created} already exists, not adding this entry.")
 
@@ -81,17 +84,18 @@ def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
     return db_connection, cursor
 
 
-def close_db(connection:sqlite3.Connection):
+def close_db(connection: sqlite3.Connection):
     connection.commit()
     connection.close()
 
 
-def setup_db(cursor:sqlite3.Cursor):
-   cursor.execute('''CREATE TABLE IF NOT EXISTS entries(
+def setup_db(cursor: sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS entries(
  EntryId INTEGER UNIQUE NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL,
  job_title TEXT NOT NULL, org_name TEXT NOT NULL, phone_num INTEGER NOT NULL,
  school_id INTEGER NOT NULL, org_site TEXT, course TEXT, speaker TEXT, siteVisit TEXT, job_shadow TEXT, 
- carreer_panel TEXT, summer_2022 TEXT, fall_2022 TEXT, spring_2023 TEXT, summer_2023 TEXT, other TEXT, permission TEXT, date_created TEXT,
+ carreer_panel TEXT, summer_2022 TEXT, fall_2022 TEXT, spring_2023 TEXT, summer_2023 TEXT, other TEXT, 
+ permission TEXT, date_created TEXT,
  created_by TEXT, date_update TEXT, updated_by TEXT);''')
 
 
