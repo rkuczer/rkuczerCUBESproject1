2cb4b61e9dbdb2bbf1b479cd5a7ad9cd628e5fca
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-02-03 17:13:27 -0500

    First test for amount of data is working now, checks for 10 entries

--- testDataRetrieve.py
+++ testDataRetrieve.py
@@ -2,10 +2,12 @@ import unittest
 from main import get_wufoo_data
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_data_num(self):
         response = get_wufoo_data()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 10)
+        data1 = response['Entries']
+        self.assertEqual(10,len(data1))
+
+    def
 
 
 if __name__ == '__main__':
