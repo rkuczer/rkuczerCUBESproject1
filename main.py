caaef891372f0d991d05a06fbd90c1bc3a40fc3f
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-02-03 14:44:33 -0500

    Added error checking so that a duplicate entryID will not be added to the database.

--- main.py
+++ main.py
@@ -34,12 +34,14 @@ def insert_db(cursor:sqlite3.Cursor, data1):
         created_by = key['CreatedBy']
         date_update = key['DateUpdated']
         updated_by = key['UpdatedBy']
-        cursor.execute("INSERT INTO entries (EntryId, first_name, last_name, job_title, org_name, phone_num, school_id, org_site, course, speaker, siteVisit, job_shadow, carreer_panel, summer_2022, fall_2022, spring_2023, summer_2023, other, permission, date_created, created_by, date_update, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (EntryId, first_name, last_name, job_title, org_name, phone_num, school_id, org_site, course, speaker, siteVisit, job_shadow, carreer_panel, summer_2022, fall_2022, spring_2023, summer_2023, other, permission, date_created, created_by, date_update, updated_by))
-
+        try:
+            cursor.execute("INSERT INTO entries (EntryId, first_name, last_name, job_title, org_name, phone_num, school_id, org_site, course, speaker, siteVisit, job_shadow, carreer_panel, summer_2022, fall_2022, spring_2023, summer_2023, other, permission, date_created, created_by, date_update, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (EntryId, first_name, last_name, job_title, org_name, phone_num, school_id, org_site, course, speaker, siteVisit, job_shadow, carreer_panel, summer_2022, fall_2022, spring_2023, summer_2023, other, permission, date_created, created_by, date_update, updated_by))
+        except sqlite3.IntegrityError:
+            print(f"Form submission with entryID={EntryId} and submission_date={date_created} already exists, not adding this entry.")
 
 def get_wufoo_data() -> dict:
     response = requests.get(url, auth=HTTPBasicAuth(apiKey, 'pass'))
-    if response.status_code != 200:  # if we don't get an ok response we have trouble
+    if response.status_code != 200:
         print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
         sys.exit(-1)
     jsonresponse = response.json()
@@ -49,15 +51,14 @@ def get_wufoo_data() -> dict:
 def main():
     data = get_wufoo_data()
     data1 = data['Entries']
-    #print(data1)
     file_to_save = open("info.txt", 'w')
     save_data(data1, save_file=file_to_save)
-
     conn, cursor = open_db("demo_db.sqlite")
     print(type(conn))
-    insert_db(cursor, data1)
     setup_db(cursor)
+    insert_db(cursor, data1)
     close_db(conn)
+
 
 def save_data(data_to_save: list, save_file=None):
     for entry in data_to_save:
@@ -81,11 +82,12 @@ def close_db(connection:sqlite3.Connection):
 
 def setup_db(cursor:sqlite3.Cursor):
    cursor.execute('''CREATE TABLE IF NOT EXISTS entries(
- EntryId INTEGER NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL,
+ EntryId INTEGER UNIQUE NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL,
  job_title TEXT NOT NULL, org_name TEXT NOT NULL, phone_num INTEGER NOT NULL,
  school_id INTEGER NOT NULL, org_site TEXT, course TEXT, speaker TEXT, siteVisit TEXT, job_shadow TEXT, 
  carreer_panel TEXT, summer_2022 TEXT, fall_2022 TEXT, spring_2023 TEXT, summer_2023 TEXT, other TEXT, permission TEXT, date_created TEXT,
  created_by TEXT, date_update TEXT, updated_by TEXT);''')
+
 
 if __name__ == '__main__':
     main()
