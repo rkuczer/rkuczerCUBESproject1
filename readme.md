82d6f43e357aada45eb99c5a572e03629beb0238
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-02-06 17:47:46 -0500

    Updated read me and started formatting testDataRetrieve.py in regard to flake8 formatting

--- readme.md
+++ readme.md
@@ -1,12 +1,19 @@
 Ryan Kuczer
 COMP 490
+
 Imports: From requests.auth import HTTPBasicAuth, import sys, and requests
 
 An API key is to be imported, a subdomain and form hash variables are needed to be changed to your own form's in the main.py file, the api key is not committed to the project on github.
 
-The project gets info from my wufoo form and saves it to a text file.
+The project gets info from my wufoo form and saves it to a sqllite table that is a part of a database.
+There is two tests implemented the first checks if the correct number of data entries are returned from the wufoo api.
+The second checks the database and table creation functions in main.py as well as the function that inserts the data into
+the database, currently the test function only works if there are multiple entries being entered into the database but still correctly
+checks for a test entry.
 
-Currently, no missing components are in Sprint 1 Project 1.
+Currently, the database layout is a single table named 'entries' with each field in the json response as its own column. 
+
+Currently, no missing components are in Sprint 1 Project 2.
 
 Wufoo form location: https://rkuczer.wufoo.com/forms/zqleboe1115c3h/
 
--- testDataRetrieve.py
+++ testDataRetrieve.py
@@ -1,6 +1,3 @@
-import pytest
-import sqlite3
-from typing import Tuple
 from main import get_wufoo_data, open_db, setup_db, close_db, insert_db
 
 
@@ -10,15 +7,9 @@ def test_data_num():
     assert len(data1) == 10, f"Expected 10 entries, but got {len(data1)}"
     return data1
 
+
 def test_db():
     conn, cursor = open_db("test_db.sqlite")
-    #not using testEntry here
-    testEntry = {'EntryId': '1', 'Field1': 'James', 'Field2': 'Pellerin', 'Field426': 'Cyber Analyst', 'Field428': 'MITRE',
-                 'Field10': '5085555555', 'Field12': '394356', 'Field430': 'mitre.com', 'Field123': '', 'Field124': '', 'Field125': '', 'Field126': ' Job Shadow', 'Field127': '',
-                'Field223': 'Summer 2022 (June 2022- August 2022)', 'Field224': '', 'Field225': '', 'Field226': '',
-                'Field227': '', 'Field423': 'Yes', 'DateCreated': '2023-01-19 21:32:01', 'CreatedBy': 'public',
-                'DateUpdated': '', 'UpdatedBy': None}
-
 
     print(type(conn))
     response = get_wufoo_data()
@@ -30,7 +21,6 @@ def test_db():
     result = cursor.fetchone()
     assert result is not None, "No entry found in the first row of the table"
     close_db(conn)
-
 
 
 if __name__ == '__main__':
