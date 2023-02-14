19ffce350fc8d21e14b307b0d9700a120210d364
rkuczer <ryan-kooz@hotmail.com>
2023-02-14 13:51:30 -0500

    More work on main window

--- view.py
+++ view.py
@@ -2,7 +2,7 @@ import sys
 import PySide6
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QTableWidget, QTableWidgetItem, \
-    QComboBox, QVBoxLayout, QLabel, QLineEdit
+    QComboBox, QVBoxLayout, QLabel, QLineEdit, QListWidget
 from PySide6.QtGui import QCloseEvent
 from main import get_wufoo_data
 
@@ -16,10 +16,19 @@ class MainWindow(QWidget):
         self.setWindowTitle("CUBES Project List")
         self.setGeometry(100, 100, 800, 500)
 
+        self.entry_list = QListWidget(self)
+        self.entry_list.setGeometry(20, 20, 760, 440)
+
         btn_quit = QPushButton('Force Quit', self)
         btn_quit.clicked.connect(QApplication.instance().quit)
         btn_quit.resize(btn_quit.sizeHint())
         btn_quit.move(700, 470)
+
+        data = get_wufoo_data()
+        data1 = data['Entries']
+
+        for entry in data1:
+            self.entry_list.addItem(entry)
 
         self.show()
 
