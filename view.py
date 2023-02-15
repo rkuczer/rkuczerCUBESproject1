1044447e06e45b206327c4a222afd16b5528244e
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-02-15 16:51:38 -0500

    Have the GUI present each entry as a button in the QListWidget currently each button only outputs text to the user. Now working on making the button display more information.

--- view.py
+++ view.py
@@ -2,9 +2,10 @@ import sys
 import PySide6
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QTableWidget, QTableWidgetItem, \
-    QComboBox, QVBoxLayout, QLabel, QLineEdit, QListWidget
+    QComboBox, QVBoxLayout, QLabel, QLineEdit, QListWidget, QListWidgetItem
 from PySide6.QtGui import QCloseEvent
 from main import get_wufoo_data
+from functools import partial
 
 
 class MainWindow(QWidget):
@@ -17,7 +18,7 @@ class MainWindow(QWidget):
         self.setGeometry(100, 100, 800, 500)
 
         self.entry_list = QListWidget(self)
-        self.entry_list.setGeometry(20, 20, 760, 440)
+        self.entry_list.setGeometry(20, 20, 300, 440)
 
         btn_quit = QPushButton('Force Quit', self)
         btn_quit.clicked.connect(QApplication.instance().quit)
@@ -28,9 +29,22 @@ class MainWindow(QWidget):
         data1 = data['Entries']
 
         for entry in data1:
-            self.entry_list.addItem(entry)
+            org_site = entry['Field430']
+            entry_button = QPushButton(org_site, self)
+
+            entry_id = entry['EntryId']
+
+            entry_button.clicked.connect(partial(self.on_entry_button_clicked, entry_id))
+
+            item = QListWidgetItem(self.entry_list)
+            item.setSizeHint(entry_button.sizeHint())
+            self.entry_list.addItem(item)
+            self.entry_list.setItemWidget(item, entry_button)
 
         self.show()
+
+    def on_entry_button_clicked(self, entry_id):
+        print(f"Entry {entry_id} button clicked")
 
 
     def closeEvent(self, event: QCloseEvent):
