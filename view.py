85b7cc82fe6f5149674ad4f01759e8e4c12e16df
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-02-15 17:25:02 -0500

    Now the Gui presents the json response on the right half of the mainwindow

--- view.py
+++ view.py
@@ -1,8 +1,9 @@
+import json
 import sys
 import PySide6
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QTableWidget, QTableWidgetItem, \
-    QComboBox, QVBoxLayout, QLabel, QLineEdit, QListWidget, QListWidgetItem
+    QComboBox, QVBoxLayout, QLabel, QLineEdit, QListWidget, QListWidgetItem, QTextEdit
 from PySide6.QtGui import QCloseEvent
 from main import get_wufoo_data
 from functools import partial
@@ -20,6 +21,9 @@ class MainWindow(QWidget):
         self.entry_list = QListWidget(self)
         self.entry_list.setGeometry(20, 20, 300, 440)
 
+        self.response_text = QTextEdit(self)
+        self.response_text.setGeometry(350, 20, 430, 440)
+
         btn_quit = QPushButton('Force Quit', self)
         btn_quit.clicked.connect(QApplication.instance().quit)
         btn_quit.resize(btn_quit.sizeHint())
@@ -29,12 +33,24 @@ class MainWindow(QWidget):
         data1 = data['Entries']
 
         for entry in data1:
-            org_site = entry['Field430']
-            entry_button = QPushButton(org_site, self)
-
+            org_name = entry['Field428']
+            entry_button = QPushButton(org_name, self)
             entry_id = entry['EntryId']
 
-            entry_button.clicked.connect(partial(self.on_entry_button_clicked, entry_id))
+            button_text = org_name
+
+            # Check if any of the fields 123-127 have a value, and add to button text if true
+            field_values = [entry['Field{}'.format(i)] for i in range(123, 128)]
+            field_values = [value for value in field_values if value]  # Remove empty values
+            if field_values:
+                field_values_str = ', '.join(field_values)
+                if org_name:
+                    button_text += ', ' + field_values_str
+                else:
+                    button_text += field_values_str
+            entry_button.setText(button_text)
+
+            entry_button.clicked.connect(partial(self.on_entry_button_clicked, entry))
 
             item = QListWidgetItem(self.entry_list)
             item.setSizeHint(entry_button.sizeHint())
@@ -43,9 +59,8 @@ class MainWindow(QWidget):
 
         self.show()
 
-    def on_entry_button_clicked(self, entry_id):
-        print(f"Entry {entry_id} button clicked")
-
+    def on_entry_button_clicked(self, entry):
+        self.response_text.setText(json.dumps(entry, indent=4))
 
     def closeEvent(self, event: QCloseEvent):
         reply = QMessageBox.question(self, 'Message', 'Are you sure you want to quit?',
